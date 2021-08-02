10b287ca11438208c7e25fc1bda473ee093117b2
tuxedotshirt <don.hagen@hotmail.com>
2021-08-02 00:22:04 -0600

    Advanced Navigation App Examples complete

--- app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -74,4 +74,9 @@ class PickupFragment : Fragment() {
         binding = null
     }
 
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_pickupFragment_to_startFragment)
+    }
+
 }
